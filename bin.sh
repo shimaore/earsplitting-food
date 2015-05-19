1f7af8025791f73c7f9cc548d632d4bbe8e14b70
Stephane Alnet <stephane@shimaore.net>
2015-05-19 09:20:05 +0200

    Show minimum proper data :)

--- bin.sh
+++ bin.sh
@@ -13,8 +13,19 @@ graph_category voice
 incoming.label Calls
 incoming.type DERIVE
 incoming.min 0
+attempts.label Attempts
+attempts.type DERIVE
+attempts.min 0
+success.label Success
+success.type DERIVE
+success.min 0
 EOT
   exit 0
 fi
 
-curl -s http://127.0.0.1:5704/statistics/incoming-calls | jq -r '"incoming.value \(.count)"'
+curl -s 'http://127.0.0.1:5704/statistics/incoming-calls' \
+  | jq -r '"incoming.value \(.count)"'
+curl -s 'http://127.0.0.1:5704/statistics/call-attempts' \
+  | jq -r '"attempts.value \(.count)"'
+curl -s 'http://127.0.0.1:5704/statistics/%5B"cause","SUCCESS"%5D' \
+  | jq -r '"success.value \(.count)"'
